2a1193cff841f5a73b3c576daddc3933a217d4f4
Sankalp-Gupta106 <sankalp106gupta@gmail.com>
2024-06-28 13:00:57 +0530

    debugging

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
     const [author, setAuthor] = useState("")
     const [title, setTitle] = useState("")
     const [blog, setBlog] = useState("")
-    const [file,setFile] = useState("")
+    const [file, setFile] = useState("")
     // const [fileName , setFileName] = useState("")
 
     // function selectFile(){
 
     // }
 
+    useEffect(() => {
+        fetch("https://blog-backend-i4ss.onrender.com/showBlogs").then(response => response.json()).then(result => console.log(result)).catch(err => console.log(err))
+    }, [])
+
 
     function handleSubmit(e) {
         e.preventDefault();
         const formdata = new FormData();
-        formdata.append('author' , author)
-        formdata.append('title' , title)
-        formdata.append('blog' , blog)
-        formdata.append('file' , file)
+        formdata.append('author', author)
+        formdata.append('title', title)
+        formdata.append('blog', blog)
+        formdata.append('file', file)
         // console.log(obj)
 
         fetch("https://blog-backend-i4ss.onrender.com/getBlog", {
             method: "POST",
             body: formdata
-        }).then(response=>response.json()).then(result=>console.log(result))
+        }).then(response => response.json()).then(result => console.log(result))
     }
     return (
         <>
@@ -33,7 +37,7 @@ function Home() {
                     <form action="" onSubmit={handleSubmit} encType='multipart/form-data'>
                         <input type="text" required placeholder="Author Name" value={author} onChange={(e) => { setAuthor(e.target.value) }} /><br />
                         <label>Author's Photo</label>
-                        <input id ="files" type ="file" onChange={(e)=>setFile(e.target.files[0])} ></input>
+                        <input id="files" type="file" onChange={(e) => setFile(e.target.files[0])} ></input>
                         <input type="text" required placeholder="Blog Title" value={title} onChange={(e) => { setTitle(e.target.value) }} /><br />
                         <textarea name="" id="" required placeholder="Blog Body" value={blog} onChange={(e) => { setBlog(e.target.value) }}></textarea><br />
                         <button type="submit">Add Blog</button>
