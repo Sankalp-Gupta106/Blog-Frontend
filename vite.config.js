589f60a28a1ec6f48b8214466bb91395cdd7bb1e
Sankalp-Gupta106 <sankalp106gupta@gmail.com>
2024-06-28 13:28:09 +0530

    applying _redirect patch

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-redirect',
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: '_redirects',
+          source: '/* /index.html 200'
+        })
+      }
+    }
+  ],
+  build: {
+    outDir: 'dist'
+  }
 })
