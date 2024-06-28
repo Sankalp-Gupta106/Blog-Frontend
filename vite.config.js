f671789ed83f3a22768886f7627742866b39d226
Sankalp-Gupta106 <sankalp106gupta@gmail.com>
2024-06-28 13:55:13 +0530

    debugging continued

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-redirect',
-      generateBundle() {
-        this.emitFile({
-          type: 'asset',
-          fileName: '_redirects',
-          source: '/* /index.html 200'
-        })
-      }
-    }
-  ],
+  plugins: [react()],
+  base: "/",
   build: {
-    outDir: 'dist'
+    outDir: "dist"
   }
 })
